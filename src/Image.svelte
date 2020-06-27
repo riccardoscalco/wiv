74ed8fe93f9c1a5c3771de209e1b31b96c846509
Riccardo Scalco <riccardo.scalco@gmail.com>
2020-06-27 23:40:53 +0200

    Show image name and size.

--- src/Image.svelte
+++ src/Image.svelte
@@ -2,6 +2,8 @@
 	import { onMount } from 'svelte';
 	export let file;
 	let src;
+	let size;
+	let name;
 
 	function readAndPreview () {
 		// Make sure `file.name` matches our extensions criteria
@@ -9,26 +11,47 @@
 			const reader = new FileReader();
 
 			reader.addEventListener("load", function () {
-				var image = new Image();
-				// image.height = 100;
-				// image.title = file.name;
-				image.src = this.result;
-				src = image.src;
+				name = file.name;
+				size = file.size;
+				src = this.result;
 			}, false);
 
 			reader.readAsDataURL(file);
 		}
 	}
 
+	function format (bytes) {
+		// International System of Units (1000 bytes = 1 kB)
+		const k = 1000;
+		return `${(bytes / k).toFixed(1)} kB`;
+	}
+
 	onMount(readAndPreview);
 </script>
 
-<img alt="just an" src={src}>
+<article>
+	<img alt="just an" src={src}/>
+	<p>{name}</p>
+	<p>{format(size)}</p>
+</article>
 
 <style>
+	article {
+		width: 100%;
+	}
+
 	img {
 		width: 100%;
 		padding: 0;
 		margin: 0;
+	}
+
+	p {
+		max-width: 100%;
+		overflow-wrap: break-word;
+		margin: 0;
+		padding: 0;
+		font-size: 12px;
+		line-height: calc(12px * 1.5);
 	}
 </style>
